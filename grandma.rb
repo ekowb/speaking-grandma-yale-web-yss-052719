64db16b196ab86779ee02fbe81d6fc91c433ea2d
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-15 17:43:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-require 'spec_helper'
+require "spec_helper"
 
 def speak_to_grandma(phrase = "Hi Grandma!")
   if phrase == "I LOVE YOU GRANDMA!"
