8a0c25da1ac0d60af340fb552705fa25eef2c0ad
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-15 17:52:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,6 +21,6 @@ def speak_to_grandma(phrase = "Hi Grandma!")
   elsif phrase.upcase == true
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
